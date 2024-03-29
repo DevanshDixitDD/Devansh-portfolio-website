f6c5425cb92976b2519b3b9cdbc3d187e3107153
Devansh Dixit <102344965+DevanshDixitDD@users.noreply.github.com>
2024-03-29 20:58:56 +0530

    Everything included

--- prismicio-types.d.ts
+++ prismicio-types.d.ts
@@ -103,6 +103,7 @@ export type BlogPostDocument<Lang extends string = string> =
   >;
 
 type HomepageDocumentDataSlicesSlice =
+  | TechListSlice
   | ExperienceSlice
   | ContentIndexSlice
   | BiographySlice
